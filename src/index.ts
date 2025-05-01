d31055f1b75b2a7b4ac84317739c4d8da2c0e5cc
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-05-01 13:34:07 -0400

    add logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
+
+app.use(logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
