b708110286d1d038d03e2b1edfa602cfe3a9e157
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-05-01 12:57:13 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+app.get('/healthz', (c) => {
+  return c.json({ status: 'ok' })
+})
+app.get('/api', (c) => {
+  return c.json({ message: 'Hello from API!' })
+})
+app.get('/api/:name', (c) => {
+  const { name } = c.req.param()
+  return c.json({ message: `Hello, ${name}!` })
+})
+
+serve({
+  fetch: app.fetch,
+  port: Number(import.meta.env?.PORT) || Number(process.env.PORT) || 3000,
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
